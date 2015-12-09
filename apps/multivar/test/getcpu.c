11d8bd887b18212fe21a24de568ec0189f862dd0
Koen Koning <koenkoning@gmail.com>
2015-12-09 14:49:31 +0100

    remove old multivar app stuff

--- apps/multivar/test/getcpu.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-
-void main()
-{
-    int id = syscall(SYS_getcpu);
-    printf("getcpu: %d\n", id);
-}
--- apps/multivar/test/gettid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-
-void main()
-{
-    int id = syscall(SYS_gettid);
-    printf("gettid: %d\n", id);
-}
