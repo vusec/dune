18092486d8bf47ebbc5b37bf3b6983f8815062cd
Koen Koning <koenkoning@gmail.com>
2016-06-21 14:31:46 +0200

    fix dune_ins script

--- dune_ins.sh
+++ dune_ins.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo rmmod dune
+lsmod | grep dune >/dev/null && sudo rmmod dune
 sudo insmod ${DIR}/kern/dune.ko
 sudo chmod 777 /dev/dune
